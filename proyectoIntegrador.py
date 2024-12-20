b03a769747f240363931b8c4d208700df6d1b8bf
gnuido <higa.guido@gmail.com>
2024-12-20 15:00:27 -0300

    Add files via upload
    Puesta en ejecución de la opción actualizar producto.

--- proyectoIntegrador.py
+++ proyectoIntegrador.py
@@ -59,18 +59,18 @@ def createProducts(marca, nombre_generico, descripcion, rubro, estante, cantidad
 	conn.commit()
 	conn.close()
 	
-def updateProducts():
+def updateProducts(update_field, update_value, update_check):
 	conn = sqlite3.connect('db_inventario.db')
 	cursor = conn.cursor()
-	query = ''''''
-	cursor.execute()
+	query = f'''UPDATE productos SET "{update_field}" = "{update_value}" WHERE id = "{update_check}"'''
+	cursor.execute(query)
 	conn.commit()
 	conn.close()
 	
-def deleteProducts(del_ID):
+def deleteProducts(delete_id):
 	conn = sqlite3.connect('db_inventario.db')
 	cursor = conn.cursor()
-	query = f'''DELETE FROM personas WHERE id = {del_ID}'''
+	query = f'''DELETE FROM productos WHERE id = "{delete_id}"'''
 	cursor.execute(query)
 	conn.commit()
 	conn.close()
@@ -78,11 +78,22 @@ def deleteProducts(del_ID):
 def searchProducts(search_field, search_value):
 	conn = sqlite3.connect('db_inventario.db')
 	cursor = conn.cursor()
-	query = f'''SELECT {search_field} FROM productos VALUE {search_value}'''
+	query = f'''SELECT * FROM productos WHERE '{search_field}' = "{search_value}"'''
 	cursor.execute(query)
 	results = cursor.fetchall()
 	conn.close()
 	return(results)
+	
+def checkNumerical(value):
+	flagError = True
+	while flagError == True:
+		try:
+			value = int(value)
+			flagError = False
+			return(value)
+		except ValueError:
+			print('La opción ingresada no es válida, intente nuevamente: ')
+			value = displayMenu()
 #%--------------------------------------
 #Inicio
 print('%'*5, '-'*5,
@@ -90,19 +101,68 @@ print('%'*5, '-'*5,
 '-' * 5, '%' * 5 + '\n')
 initializeTable()
 
-option = displayMenu()
-
-'''marca = 'Rucci'
-nombre_generico = 'Llave Stilson'
-descripcion = 'Llave Stilson tipo sueca, acero forjado'
-rubro = 'Ferretería'
-estante = 28
-cantidad = 18
-precio = 120500
-proveedor = 'Rucci S.R.L.'
-createProducts(marca, nombre_generico, descripcion, rubro, estante, cantidad, precio, proveedor)
-'''
-search_field = 'marca'
-search_value = 'Rucci'
-result = searchProducts(search_field, search_value)
-print(result)
+option = checkNumerical(displayMenu())
+#verificar opción válida
+#%--------------------------------------
+while option != 7 & (0 < option < 7) == True:
+	#%----------------
+	if option == 1:
+		listProducts = readProducts()
+		if len(listProducts) == 0:
+			print('La lista de productos se encuentra vacía.')
+			option = checkNumerical(displayMenu())
+		else:
+			print(listProducts)
+			option = checkNumerical(displayMenu())
+	#%----------------
+	elif option == 2:
+		marca = input('Marca comercial del producto: ')
+		nombreGenerico = input('Nombre genérico del producto: ')
+		descripcion = input('Decripción y especificaciones del producto: ')
+		rubro = input('Rubro: ')
+		estante = input('Nº de estante de reposición (dejar vacío de ser necesario): ')
+		cantidad = input('Cantidad en stock: ')
+		precio = input('Precio por unidad (dejar vacío de ser necesario): ')
+		proveedor = input('Proveedor/es (dejar vacío de ser necesario): ')
+		createProducts(marca, nombreGenerico, descripcion, rubro, estante, cantidad, precio, proveedor)
+		print('El producto se añadió correctamente')
+	#%----------------
+	elif option == 3:
+		update_check = input('ID del producto a modificar: ')
+		update_field = input('Campo a modificar: ')
+		update_value = input('Nuevo valor: ')
+		updateProducts(update_field, update_value, update_check)
+	#%----------------
+	elif option == 4:
+		delete_id = input('ID del producto a eliminar: ')
+		deleteProducts(delete_id)
+	#%----------------
+	elif option == 5:
+		search_field = input('Campo de entrada: ')
+		search_value = input('Valor de busqueda: ')
+		searchProducts(search_field, search_value)
+	#%----------------
+	elif option == 6:
+		print('\n' + '-' * 80,
+		'\n-> Las opciones de eliminar y actualizar productos requieren conocer de antemano la ID del producto en inventario',
+		'\n-> El nombre genérico de producto refiere a la especie, por ejemplo: taladro, leche en polvo, mesa, etc.',
+		'\n-> El precio se debe expresar en pesos argentinos (ARS)',
+		'\n-> Para recibir asistencia personalizada consulte con su administrador o complete un reporte en:',
+		'https://github.com/gnuido/Proyecto-Integrador/issues')
+	#%----------------
+	else:
+		indiceError = 0		
+		#contador de intentos inválidos
+		if indiceError < 3:
+			print('Opción inválida, por favor, intente nuevamente')
+			indiceError += 1
+		else:
+			print('El valor que ingresado no es válido.',
+			'Por favor, ingrese únicamente el dìgito correspondiente a la opción')
+			indiceError = 0
+#%----------------
+else:
+		print('Finalizando...',
+		'\n%' + '-' * 82)
+#%--------------------------------------
+#fin
